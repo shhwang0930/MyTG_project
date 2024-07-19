16befe38ecf9b7b8086ff63e3c5d119deea4a75b
shhwang0930 <shhwang0930@gamail.com>
2024-07-19 16:51:56 +0900

    start mytg

---
+++ src/main/java/com/shhwang0930/mytg/MytgApplication.java
@@ -0,0 +1,13 @@
+package com.shhwang0930.mytg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shhwang0930/mytg/MytgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shhwang0930.mytg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
